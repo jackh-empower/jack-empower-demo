491eb76eef92ff2662e32160e85b98b7a6f124bc
HetheriJ0523 <HetheriJ0523@BHY4QG3>
2023-03-28 14:29:56 +0200

    Add project files.

---
+++ jackh_empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jackh_empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("jackh_empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8AEB3B75-6A80-44E4-B0BA-5638E902F9C5")]
+[assembly: AssemblyVersion("1.0.0.0")]
