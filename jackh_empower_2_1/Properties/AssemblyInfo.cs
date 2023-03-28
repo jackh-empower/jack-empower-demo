1c5a621976b49ee8ed5786823af95fe14854ef07
Jack Hetherington <Jack.Hetherington@paramount.com>
2023-03-28 16:44:45 +0200

    Order stuff

---
+++ jackh_empower_2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("jackh_empower_2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("jackh_empower_2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("70AB0585-DBC8-4C42-A038-75A7051D9109")]
+[assembly: AssemblyVersion("1.0.0.0")]
